6025c654371a39e93a72ed44cc816fcc1824f757
Andres MRM <andres@inventati.org>
2015-08-21 17:46:10 -0300

    needed for okfn-brasil/cuidando2#2

--- tagarela/views.py
+++ tagarela/views.py
@@ -17,17 +17,18 @@ api = Api(version='1.0',
           description='A commenting microservice. All non-get operations '
           'require a micro token.')
 
+parser = api.parser()
+parser.add_argument('token', location='json', help='The task details')
+parser.add_argument('text', location='json')
+
 
 @api.route('/thread/<string:thread_name>/add')
 class AddComment(Resource):
 
-    parser = api.parser()
-    parser.add_argument('token', location='json')
-    parser.add_argument('text', location='json')
-
+    @api.doc(parser=parser)
     def post(self, thread_name):
         '''Add a comment to thread.'''
-        args = self.parser.parse_args()
+        args = parser.parse_args()
         decoded = decode_token(args['token'], sv, api)
         author_name = decoded['username']
 
@@ -64,38 +65,27 @@ class AddComment(Resource):
 @api.route('/thread/<string:thread_name>/<int:comment_id>/delete')
 class DeleteComment(Resource):
 
-    parser = api.parser()
-    parser.add_argument('token', location='json')
-
     def delete(self, thread_name, comment_id):
         '''Delete a comment from a thread. Returns thread.'''
-        args = self.parser.parse_args()
+        args = parser.parse_args()
         decoded = decode_token(args['token'], sv, api)
-        author_name = decoded['username']
-
-        comment = db.session.query(Comment).filter_by(id=comment_id).one()
-        print(comment)
-        try:
-            comment = db.session.query(Comment).filter_by(id=comment_id).one()
-        except NoResultFound:
-            api.abort(404, 'Comment not found')
-
-        if comment.author.name != author_name:
-            api.abort(400, 'You seem not to be the author of this comment...')
-
+        comment = check_comment_author(comment_id, decoded['username'])
         db.session.delete(comment)
         db.session.commit()
-
         return get_thread_comments(thread_name)
 
 
-
-@api.route('/thread/<string:thread_name>/<int:comment>/edit')
+@api.route('/thread/<string:thread_name>/<int:comment_id>/edit')
 class EditComment(Resource):
 
-    def put(self, thread_name, comment):
+    def put(self, thread_name, comment_id):
         '''Edit a comment in a thread.'''
-        pass
+        args = parser.parse_args()
+        decoded = decode_token(args['token'], sv, api)
+        comment = check_comment_author(comment_id, decoded['username'])
+        comment.text = args['text']
+        db.session.commit()
+        return get_thread_comments(thread_name)
 
 
 @api.route('/thread/<string:thread_name>')
@@ -127,8 +117,15 @@ def get_thread_comments(thread_name):
     }
 
 
-# @api.route('/<string:thread_name>/<int:comment>')
-# class GetComment(Resource):
+def check_comment_author(comment_id, author_name):
+    '''Checks if comment_id exists and author_name is its author.
+    Returns the comment.'''
+    try:
+        comment = db.session.query(Comment).filter_by(id=comment_id).one()
+    except NoResultFound:
+        api.abort(404, 'Comment not found')
 
-#     def get(self, thread_name, comment):
-#         pass
+    if comment.author.name != author_name:
+        api.abort(400, 'You seem not to be the author of this comment...')
+
+    return comment
