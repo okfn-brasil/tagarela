d327d84f5a87c63e22b7d56ffa904fa6846cd1b0
Andres MRM <andres@inventati.org>
2015-08-22 10:20:05 -0300

    example config to report to email

--- settings/local_settings.example.py
+++ settings/local_settings.example.py
@@ -1,1 +1,24 @@
 SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@localhost/{database}'
+
+
+# ----------------------------- #
+# Report to email functionality #
+# ----------------------------- #
+
+__username__ = '{email_username}@gmail.com'
+SECRET_KEY = '{secret-key}'
+
+# This is the address where this service is hosted. It will be used in the
+# links sent to admin e-mail to delete reported tokens.
+HOSTED_ADDRESS = 'http://{address}:{port}'
+
+# These are used to send e-mails to the admin when a comment is reported.
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = __username__
+MAIL_PASSWORD = '{email_password}'
+SENDER_NAME = __username__
+
+# Admins e-mails. They will receive links to delete te reported comments.
+ADMIN_EMAILS = [__username__]
